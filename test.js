891ceb22f23f77a4fbdebefcfe2386332dd832c9
Perplexiz <34402882+Perplexiz@users.noreply.github.com>
2022-03-28 20:36:14 -0400

    my first check in

---
+++ test.js
@@ -0,0 +1,27 @@
+x = 1
+y =2
+z = x+y
+console.log(z)
+
+firstName = "Aneesh Sharma"
+console.log(firstName)
+
+function sayHello(){
+    console.log("hello java script")
+}
+
+sayHello()
+
+var talk = function sayHello2(){
+    console.log("hello java script 2")
+}
+
+talk()
+
+myFirstFunction3 = () => {
+    console.log("hello java script 3")
+}
+myFirstFunction3()
+
+myFirstFunction4 = () => console.log("hello java script 4")
+myFirstFunction4()
